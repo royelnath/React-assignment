1ce80b5f8a2318c0995488d50843002cfe321967
royelnath <royelnath123@gmail.com>
2026-04-06 07:52:10 +0530

    fixed routing

--- src/App.jsx
+++ src/App.jsx
@@ -7,27 +7,22 @@ import Skills from './pages/Skills'
 import Weather from './pages/Weather'
 import Err from './pages/Err'
 import Submit from './pages/Submit'
-// import Navbar from './Component/Navbar'
+import Navbar from './Component/Navbar'
 
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/skills">Skills</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/education">Education</Link>
-          <Link to="/weather">Weather</Link>
-        </nav>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path='/skills' element={<Skills />} />
-          <Route path='/contact' element={<Contact />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path='/education' element={<Education />} />
+          <Route path='/skills' element={<Skills />} />
           <Route path='/weather' element={<Weather />} />
-
+          <Route path='*' element={<Err />} />
+          <Route path='/submit' element={<Submit />} />
         </Routes>
       </BrowserRouter>
     </>
